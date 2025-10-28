818564836b4b5a3ba3f61b7c0d4aa916d550af50
smaru92 <koprad1@gmail.com>
2025-10-28 13:02:58 +0900

    [fix] 이미지 버전추가

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,11 @@ class AppServiceProvider extends ServiceProvider
         $this->callAfterResolving(Schedule::class, function (Schedule $schedule) {
             $this->app->make(\App\Console\Kernel::class)->schedule($schedule);
         });
+
+        // helpers.php 파일 수동 로드 (composer autoload 백업)
+        $helpersPath = app_path('helpers.php');
+        if (file_exists($helpersPath)) {
+            require_once $helpersPath;
+        }
     }
 }
