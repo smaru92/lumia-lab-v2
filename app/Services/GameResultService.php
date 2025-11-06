f10749d9da16b0e1350567c5c848db46bb0534ef
smaru92 <koprad1@gmail.com>
2025-11-06 10:47:11 +0900

    [fix] 메타스코어 계산 가중치 변경

--- app/Services/GameResultService.php
+++ app/Services/GameResultService.php
@@ -862,7 +862,7 @@ class GameResultService
         $synergy = sqrt($pickNormalized * $perfNormalized) * 3.0; // 기하평균 사용
 
         // 최종 메타 점수
-        $metaScore = ($performanceScore * 1.0 + $pickRateScore * 2.2 + $synergy) / 2;
+        $metaScore = ($performanceScore * 1.2 + $pickRateScore * 2.7 + $synergy) / 2;
 
         // 디버깅용 변수 재할당
         $P_raw = $performanceScore / $lowPickPenalty;
@@ -973,7 +973,7 @@ class GameResultService
         $synergy = sqrt($pickNormalized * $perfNormalized) * 3.0; // 기하평균 사용
 
         // 최종 메타 점수
-        $metaScore = ($performanceScore * 1.0 + $pickRateScore * 2.2 + $synergy) / 2;
+        $metaScore = ($performanceScore * 1.2 + $pickRateScore * 2.7 + $synergy) / 2;
 
         // 티어 분류
         $metaTier = match (true) {
