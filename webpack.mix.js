3e1862dce283638902c39475505237938066ce34
smaru92 <koprad1@gmail.com>
2025-11-24 14:25:18 +0900

    [fix] 장비 쿼리 최적화, js파일 캐시제거

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ])
+    .version(); // 파일에 해시 추가하여 캐시 무효화
