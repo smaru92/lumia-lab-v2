73362ac859e1ac6a42b987608ff281378f8a8f11
smaru92 <koprad1@gmail.com>
2025-11-28 10:55:42 +0900

    [fix] 메인페이지 기준티어 변경

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -303,7 +303,7 @@
             </thead>
             <tbody>
                 @foreach($buffedCharacters as $index => $item)
-                <tr data-href="/detail/{{ $item['character_name'] }}-{{ $item['weapon_type_en'] ?? $item['weapon_type'] }}?min_tier=Diamond&version={{ $latestVersion->version_season }}.{{ $latestVersion->version_major }}.{{ $latestVersion->version_minor }}" class="buffed-row {{ $index >= 5 ? 'hidden-row' : '' }}">
+                <tr data-href="/detail/{{ $item['character_name'] }}-{{ $item['weapon_type_en'] ?? $item['weapon_type'] }}?min_tier=Meteorite&version={{ $latestVersion->version_season }}.{{ $latestVersion->version_major }}.{{ $latestVersion->version_minor }}" class="buffed-row {{ $index >= 5 ? 'hidden-row' : '' }}">
                     <td>
                         <div class="character-cell-content">
                             @php
